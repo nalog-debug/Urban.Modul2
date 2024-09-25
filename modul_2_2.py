0f665388d7872b1cb50b7808d16569fc2c73fe3c
nalog-debug <miha_s_fc_98@mail.ru>
2024-09-25 05:36:55 +0300

    "Условная конструкция. Операторы if, elif, else."(2)

--- modul_2_2.py
+++ modul_2_2.py
@@ -1,9 +1,9 @@
 first = input()
-second =  input()
+second = input()
 third = input()
-if first == second == third :
+if first == second == third:
     print(3)
 elif first == second or first == third or second == third:
-     print(2)
-elif first != second != third :
-     print(0)
+    print(2)
+elif first != second != third:
+    print(0)
